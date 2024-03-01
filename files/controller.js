a8e6e1bd84f1163726e3f672001e27914d2878fb
Stefan Simic <stefansim2000@gmail.com>
2024-03-01 16:36:53 +0100

    add changelistener for detail view

--- files/controller.js
+++ files/controller.js
@@ -1,33 +1,66 @@
 let dataID = 0;
 let selectedPerson;
 
+class Observable {
+    constructor(functionThatTakesObserver){
+      this._functionThatTakesObserver = functionThatTakesObserver;
+    }
+
+    subscribe(observer) {
+      return this._functionThatTakesObserver(observer)
+    }
+}
+
+class PersonObserver {
+    firstname;
+    name;
+    plz;
+
+    constructor(firstname, name, plz) {
+        this.firstname = firstname;
+        this.name = name;
+        this.plz = plz;
+    }
+
+    next(data) {
+        this.firstname.value = data?.firstName ? data?.firstName : "";
+        this.name.value = data?.name ? data?.name : "";
+        this.plz.value = data?.plz ? data?.plz : "";
+    }
+}
+
+let personObserver;
+
 function addData(dataTableBody){
     const trID = "TR-" + dataID;
-    console.log("Pressed!");
+    
     dataTableBody.innerHTML += "<tr id='" + trID + "'" +
-        "onClick='loadPerson(\"" +
-        trID +
-        "\")'>\n " +
-        "                    <td></td>\n" +
-        "                    <td></td>\n" +
-        "                    <td></td>\n" +
-        "                </tr>"
-    console.log(dataTableBody.innerHTML);
+        "onClick='loadPerson(\"" + trID + "\")'>\n " +
+        "<td></td>\n" +
+        "<td></td>\n" +
+        "<td></td>\n" +
+        "</tr>";
+        
     savePerson("", "", "", trID)
+
+    selectedPerson = null;
+    personObserver.next(selectedPerson);
     dataID++;
 }
 
 function loadPerson(trID){
-    console.log("selected tr:" + trID );
     selectedPerson = getPersonByID(trID);
+    personObserver.next(selectedPerson);
 }
 
-function addDetailViewChangeListener(firstname, name, plz){
-    // Object.observe(selectedPerson, function(changes) {
-    //     console.log(changes);
-    // });
+function addDetailViewChangeListener(firstname, name, plz) {
+    const observerOfPerson = new Observable(observer => {
+        personObserver = new PersonObserver(firstname, name, plz);
+    })
+      
+    observerOfPerson.subscribe(personObserver)
 }
 
-function submitPerson(firstname, name, plz){
+function submitPerson(firstname, name, plz) {
     savePerson(firstname.value, name.value, plz.value, selectedPerson.id);
 }
